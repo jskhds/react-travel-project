88ded409f6f363a074d74b7531b1ac687d04b8de
jskhds <2987163443a@gmail.com>
2022-02-10 17:33:58 +0800

    搭建购物车和订单页面的路由守卫

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import styles from "./App.module.css";
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes, useNavigate , useLocation} from "react-router-dom";
 import {
   HomePage,
   SignInPage,
@@ -14,18 +14,25 @@ import { ShoppingCart } from "./pages/shoppingCart";
 import { useDispatch } from "react-redux"
 import { getShoppingCart, clearShoppingCartItem, addShoppingCartItem } from "./redux/shoppingCart/slice"
 
-// // component 路由所指的页面，isAuthenticated 是否登录，...res 传入其它属性
-// const PrivateRoute = ({ element, isAuthenticated, ...rest }) => {
-//     const routeComponent = (props) => {
-//     return isAuthenticated ? (
-//       React.createElement(element, props)
-//     ) : (
-//       <Navigate to= "/signIn"   />
-//     ); 
-//   }
-//   return <Route element={routeComponent} {...rest} />;
 
-// }
+const PrivateRoute = (props: { children: React.ReactNode }): JSX.Element => {
+  const { children } = props
+  const jwt = useSelector((s) => s.user.token);
+  // Up to you specific app to decide how to get this value
+  const isAuthenticated: boolean = jwt !== null;
+  const location = useLocation()
+
+  return isAuthenticated ? (
+    <>{children}</>
+  ) : (
+    <Navigate
+      replace={true}
+      to="/signIn"
+      state={{ from: `${location.pathname}${location.search}` }}
+    />
+  )
+}
+ 
 
 
 
@@ -57,15 +64,17 @@ function App() {
             <Route path=":keywords" element={<SearchPage />} />
           </Route>
 
-          <Route
+          {/* 未登录的用户不能进入 购物车和订单页面，需要跳转到登录页面 */}
 
+{/*           
+          <Route
             path="/shoppingCart"
             element={< ShoppingCart />} />
-
           <Route
-
             path="/placeOrder"
-            element={< PlaceOrderPage/>} />
+            element={< PlaceOrderPage/>} /> */}
+            <Route path="/shoppingCart" element={<PrivateRoute> <ShoppingCart /> </PrivateRoute>}/>
+            <Route path="/placeOrder" element={<PrivateRoute> <PlaceOrderPage /> </PrivateRoute>}/>
 
           <Route path="*" element={<h1>404 Not Found! 页面去火星了</h1>} />
         </Routes>
